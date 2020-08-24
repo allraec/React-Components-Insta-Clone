a25e06ea7c3eed6f6d269c53f101db1158236e8f
Allison Castaneda <allraec@yahoo.com>
2020-08-23 20:04:06 -0500

    1

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,7 +3,7 @@ import Comment from './Comment';
 import './Comments.css';
 
 const Comments = props => {
-  // 🔥 Make sure the parent of Comments is passing the right props!
+  // 🔥 Make sure the parent of Comments is passing the right props!.
   const { comments } = props;
 
   return (
