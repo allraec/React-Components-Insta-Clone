42405a7a1cf7f523c09febbce91e5746fb1a92c4
Allison Castaneda <allraec@yahoo.com>
2020-08-24 20:30:09 -0500

    MVP

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -9,21 +9,14 @@ const Post = props => {
 
   return (
     <div className='post-border'>
-      <PostHeader
-        username={post.username}
-        thumbnailUrl={post.thumbnailUrl}
-      />
+      <PostHeader username={post.username} thumbnailUrl={post.thumbnailUrl}/>
       <div className='post-image-wrapper'>
-        <img
-          alt='post thumbnail'
-          className='post-image'
-          src={post.imageUrl}
-        />
+        <img alt='post thumbnail' className='post-image' src={post.imageUrl}/>
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection numberOfLikes={post.likes} likePost={() => likePost(post.id)} />
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments}/>
     </div>
   );
 };
