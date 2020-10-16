8cd2615ca3b1c54c429f298281b05e1002eb88bf
Allison Castaneda <allraec@yahoo.com>
2020-10-15 20:26:45 -0500

    shows posts and likes

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Post from './Post';
 import './Posts.css';
 
-const Posts = (props) => {
+const Posts = ({ likePost, posts }) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
-
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map((post) => {
+          return (
+            <Post key={posts.id} likePost={likePost} post={post} />
+          )
+        }
+        )
+      }
     </div>
   );
 };
